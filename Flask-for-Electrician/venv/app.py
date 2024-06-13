d73faa64908aeee211a7b81ac5216fcfa2178ce0
Akshat Srivastava <113674819+akshatgithubgre@users.noreply.github.com>
2024-06-13 09:11:44 +0530

    flask task

--- Flask-for-Electrician/venv/app.py
+++ Flask-for-Electrician/venv/app.py
@@ -1,3 +1,7 @@
 from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app=Flask(__name__)
+@app.route('/')
+def index():
+    return redirect('welcome')
 from controller import *
--- Flask-for-Electrician/venv/controller/user_controller.py
+++ Flask-for-Electrician/venv/controller/user_controller.py
@@ -1,13 +1,20 @@
 from app import app
 from model.user_model import user_model
-from flask import request,send_file
+from flask import Flask, request, render_template, redirect, url_for
 from datetime import datetime
 obj=user_model()
 
 
-@app.route("/welcome",methods=["POST"])
+@app.route("/welcome", methods=["GET", "POST"])
 def user_welcome_controller():
-    return obj.user_welcome_model(request.form)
-@app.route("/signup_signin",methods=["POST"])
+    if request.method == 'POST':
+        message = obj.user_welcome_model(request.form)
+        return render_template('welcome.html', message=message)
+    return render_template('welcome.html')
+@app.route("/signup_signin", methods=["GET", "POST"])
 def user_signup_signin_controller():
-    return obj.user_singup_signin(request.form)
+    if request.method == "POST":
+        message = obj.user_signup_signin_model(request.form)
+        if message:
+            return render_template('signup_signin.html', message=message)
+    return render_template('signup_signin.html')
